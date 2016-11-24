8cc8a856e931314645a14c63e0d9fde0350b0260
Mike Bundy <mike.bundy@gmail.com>
2016-11-23 19:17:42 -0600

    trying to figure out menu accelerator

---
+++ MenuDemo/MenuDemo/src/main.js
@@ -0,0 +1,35 @@
+﻿const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+
+app.on('ready', _ => {
+    new BrowserWindow();
+
+    const template = [
+        {
+            label: "&File",
+            accelerator: "Alt+f",
+            submenu: [{
+                label: 'About',
+                click: _ => {
+                    console.log('clicked');
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Alt+W',
+                click: _ => {
+                    app.quit();
+                }
+            }]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+});
